1b6a35d171e245e6b7a77856ba25d3300f3f4515
Rohan2104 <maindarkarrohan@yahoo.com>
2023-07-02 21:29:14 +0530

    Add new design

---
+++ RGB_Star_Desgin.py
@@ -0,0 +1,22 @@
+
+
+import turtle
+import colorsys
+s= turtle.Screen().bgcolor('black')
+t= turtle.Turtle()
+t.speed(0)
+t.pensize(1)
+
+n=10
+h=0
+
+for i in range(90):
+    c=colorsys.hsv_to_rgb(h,1,0.8)
+    h+= 1/n
+    t.color(c)
+    t.forward(i * 5)
+    t.left(140)
+    
+    
+turtle.done()   
+
